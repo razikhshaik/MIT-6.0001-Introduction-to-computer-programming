4c48b37521c93d7dbbc8541ae22a8b71a9b08151
Razikh Shaik <razikhshaik@gmail.com>
2018-11-18 01:30:20 -0500

    Added PS0 Solution/Comments and explanation

--- Assignments/Problem Set 0/ps0solution.py
+++ Assignments/Problem Set 0/ps0solution.py
@@ -1,1 +1,11 @@
+# Author: Razikh Shaik
+# Problem Set 0
+# This is to just test whether you have anaconda 
+# python installed and all the necessary files 
+# are present
+# Just run this file or pkgtest.py 
+# you will get a graph and everything is working 
+# fine
+
+
 import pkgtest
